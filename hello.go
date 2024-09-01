2b6e32bfcb134b2c1db8a2354a104a26bd7f7b9e
Lucas Gama <lucasgdon@gmail.com>
2024-09-01 20:10:57 -0300

    Captação de input com Scan, sem precisar explicitar o tipo com Scanf

--- hello.go
+++ hello.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	nome := "Lucas"
-	idade := 27
 	versao := 1.1
 
-	fmt.Println("Olá, Sr.", nome, ", sua idade é: ", idade)
+	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão: ", versao)
+
+	// Renderização do menu
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi: ", comando)
+
 }
