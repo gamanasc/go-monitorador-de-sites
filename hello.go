47cf7784a7caa3da603d76e839f03d55088e3306
Lucas Gama <lucasgdon@gmail.com>
2024-09-01 18:41:59 -0300

    Primeiros passos com Go, variáveis e Println

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Lucas"
+	var versao float32 = 1.1
+	var idade int = 27
+
+	fmt.Println("Olá, Sr.", nome, ", sua idade é: ", idade)
+	fmt.Println("Este programa está na versão: ", versao)
+}
