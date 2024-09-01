e7ee2814f803ac8e6af0399fa8ce5d2d9fd3f420
Lucas Gama <lucasgdon@gmail.com>
2024-09-01 20:34:59 -0300

    Modularizando código em funções

--- hello.go
+++ hello.go
@@ -1,23 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Lucas"
-	versao := 1.1
 
-	fmt.Println("Olá, Sr.", nome)
-	fmt.Println("Este programa está na versão: ", versao)
+	exibeIntroducao()
+	exibeMenu()
 
-	// Renderização do menu
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
-
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O comando escolhido foi: ", comando)
+	comando := leComando()
 
 	// Em Go, o switch não precisa de break
 	switch comando {
@@ -27,8 +20,32 @@ func main() {
 		fmt.Println("Exibindo logs...")
 	case 0:
 		fmt.Println("Saindo...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
 
+}
+
+func exibeIntroducao() {
+	nome := "Lucas"
+	versao := 1.1
+
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Este programa está na versão: ", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi: ", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
 }
